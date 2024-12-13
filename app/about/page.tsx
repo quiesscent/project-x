ae7e1dc2f2ff94e8ceb17b26ce1bef03fb604a1d
ephesianslewis <ephesianslewis@gmail.com>
2024-12-13 10:45:07 +0000

    Update Using IDX

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Card, CardHeader, CardFooter, Image, Button } from "@nextui-org/react";
 
 import { title } from "@/components/primitives";
@@ -5,17 +6,25 @@ import { title } from "@/components/primitives";
 export default function AboutPage() {
   return (
     <div>
-      <h1 className={title()}>About</h1>
-      <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
-        <Card className="col-span-12 sm:col-span-4 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">
-              What to watch
-            </p>
-            <h4 className="text-white font-medium text-large">
-              Stream the Acme event
-            </h4>
-          </CardHeader>
+      <h1 className="mb-4 text-bold font-bold text-[50px]">Gallery</h1>
+      <div className="gap-1 grid grid-cols-3 sm:grid-cols-1 lg:grid-cols-3">
+        <Card className="h-[300px] w-[400px]">
+          <Image
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-[200%] h-full object-cover"
+            src="https://www.railaodinga4auc.com/blogs/5.jpeg"
+          />
+        </Card>
+        <Card className="h-[300px] w-[400px]">
+          <Image
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://www.railaodinga4auc.com/gallery/9.jpeg"
+          />
+        </Card>
+        <Card className="h-[300px] w-[400px]">
           <Image
             removeWrapper
             alt="Card background"
@@ -23,15 +32,17 @@ export default function AboutPage() {
             src="https://www.railaodinga4auc.com/blogs/5.jpeg"
           />
         </Card>
-        <Card className="col-span-12 sm:col-span-4 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">
-              Plant a tree
-            </p>
-            <h4 className="text-white font-medium text-large">
-              Contribute to the planet
-            </h4>
-          </CardHeader>
+
+        <Card className="h-[300px] w-[400px]">
+          <Image
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://www.railaodinga4auc.com/gallery/23.jpeg"
+          />
+        </Card>
+
+        <Card className="h-[300px] w-[400px]">
           <Image
             removeWrapper
             alt="Card background"
@@ -39,15 +50,44 @@ export default function AboutPage() {
             src="https://www.railaodinga4auc.com/blogs/5.jpeg"
           />
         </Card>
-        <Card className="col-span-12 sm:col-span-4 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">
-              Supercharged
-            </p>
-            <h4 className="text-white font-medium text-large">
-              Creates beauty like a beast
-            </h4>
-          </CardHeader>
+
+        <Card className="h-[300px] w-[400px]">
+          <Image
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://www.railaodinga4auc.com/blogs/5.jpeg"
+          />
+        </Card>
+
+        <Card className="h-[300px] w-[400px]">
+          <Image
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://www.railaodinga4auc.com/blogs/5.jpeg"
+          />
+        </Card>
+
+        <Card className="h-[300px] w-[400px]">
+          <Image
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://www.railaodinga4auc.com/blogs/5.jpeg"
+          />
+        </Card>
+
+        <Card className="h-[300px] w-[400px]">
+          <Image
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://www.railaodinga4auc.com/blogs/5.jpeg"
+          />
+        </Card>
+
+        <Card className="h-[300px] w-[400px]">
           <Image
             removeWrapper
             alt="Card background"
